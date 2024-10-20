3a022d3a45e89f72117760927fd461a15971b0cf
manbeao <142187096+manbeao@users.noreply.github.com>
2024-10-20 22:52:26 +0900

    Update employee.sql

--- MYSQL/employee.sql
+++ MYSQL/employee.sql
@@ -126,7 +126,7 @@ WHERE JOB_CODE = 'J2' AND SALARY >2000000 OR JOB_CODE = 'J7';
 
 -- 15. J7 직급이거나 J2 직급인 직원들 중 급여가 200만원 이상인 직원의 이름, 급여, 직급코드를 조회하세요
 SELECT
-		EMP_NAME,
+	EMP_NAME,
         SALARY,
         JOB_CODE
 FROM employee
